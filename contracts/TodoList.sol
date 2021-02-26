38c7c1b7597653541354661c442cc55837b07908
Pankaj Sindagi <pankajsindagi@Pankajs-MacBook-Air.local>
2021-02-26 19:14:20 +0530

    Project Setup

---
+++ contracts/TodoList.sol
@@ -0,0 +1,12 @@
+// pragma solidity ^0.5.5;
+
+// contract TodoList {
+//     unit public taskCount = 0;
+// }
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+
+contract TodoList {
+    uint public taskCount = 0;
+}
